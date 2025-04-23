0a77b06e0baf086f50cffa55210b330f098e392c
gaoxh <2036902768@qq.com>
2025-04-23 21:39:01 +0800

    fix: 总体测试完成

---
+++ src/package/components/hy-status-bar/hy-status-bar.vue
@@ -0,0 +1,13 @@
+<template>
+  <view
+    :style="[style]"
+    class="hy-status-bar"
+    :class="[isH5 && 'hy-safe-area-inset-top']"
+  >
+    <slot />
+  </view>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/package/components/hy-status-bar/props.ts
@@ -0,0 +1,21 @@
+import type IProps from "./typing";
+import { ColorConfig } from "../../config";
+
+const defaultProps: IProps = {
+  modelValue: 0,
+  blockSize: 18,
+  min: 0,
+  max: 100,
+  step: 1,
+  activeColor: ColorConfig.primary,
+  inactiveColor: "#c0c4cc",
+  blockColor: "#ffffff",
+  showValue: false,
+  disabled: false,
+  useNative: false,
+  height: "2px",
+  rangeValue: [0, 0],
+  isRange: false,
+};
+
+export default defaultProps;
